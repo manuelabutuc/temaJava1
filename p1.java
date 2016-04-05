4991d67d2c0fe0035a12475e2e132d2b3f04e824
Manu <gavrismanuela1993@gmail.com>
2016-04-06 00:03:45 +0300

    rezolvat cu while

--- p1.java
+++ p1.java
@@ -4,28 +4,33 @@ import java.util.Scanner;
  */
 public class p1 {
 
+
     public static void main(String[] args) {
 
          /*  1.Se da un sir de numere ordonate crescator.
         Se va determina daca un numar n exista in sir, si daca exista se va afisa pozitia pe care s-a gasit, altfel se va afisa -1. */
 
         int[] sir = {1, 3, 5, 6, 7, 8, 11, 13, 14, 26, 100};
-        System.out.println("Introduceti numarul cautat:");
+
+        System.out.print("Introduceti numarul cautat:");
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
-        int i=0;
-        boolean test=false;
 
-        while (i <sir.length){
-            if (n == sir[i]) {
-                System.out.println("Numarul cautat se afla pe pozitia " + i);
-                test=true;
+        int i = 0;
+        boolean test = true;
+
+        while (test && i<sir.length) {
+            if (sir[i] == n) {
+
+                System.out.println("Numarul a fost gasit la pozitia: " + i + " a sirului!");
+                test = false;
             }
             i++;
-        }
 
-        if(test==false) {
+        }
+        if (test) {
             System.out.println("-1");
+
         }
     }
 }
