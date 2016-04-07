548a06fd329e77c787518002130adbe88a9a062d
Manu <gavrismanuela1993@gmail.com>
2016-04-07 10:07:57 +0300

    modificata pentru perechile alaturate

--- p7.java
+++ p7.java
@@ -4,16 +4,14 @@
 public class p7 {
     public static void main(String[] args) {
         //Se da un sir de numere. Sa se afiseze perechile de numere pentru care  primul numar este mai mare decat al doilea.
-        int[] list = {2, 3, 5, 6, 1};
+        int[] list = {2, 3, 5, 5, 6, 1, 34, 12, 43, 7, 22, 9};
         int i;
-        int j;
         System.out.println("Perechile de numere sunt:");
-        for (i = 0; i < list.length; i++) {
-            //System.out.println(list[i]);
-            for (j = 0; j < list.length; j++) {
-                if (list[i] > list[j]) {
-                    System.out.print("(" + list[i] + "," + list[j] + ")" + "  ");
-                }
+        for (i = 0; i < list.length - 1; i++) {
+            if (list[i] > list[i + 1]) {
+                System.out.print("(" + list[i] + "," + list[i + 1] + ")" + "  ");
+            } else if (list[i] < list[i + 1]) {
+                System.out.print("(" + list[i + 1] + "," + list[i] + ")" + "  ");
             }
         }
     }
