4ad5fd5cbb03ac6518466e66fe7caf77a67f30b6
Manu <gavrismanuela1993@gmail.com>
2016-04-07 10:20:58 +0300

    modificat, fara pow

--- p8.java
+++ p8.java
@@ -5,22 +5,26 @@ import java.util.Scanner;
  */
 public class p8 {
     public static void main(String[] args) {
+        
         /*Se da un numar intreg x si un numar intreg n. Sa se calculeze urmatoarea suma:
 suma = 1+x+x la puterea 2 + x la puterea 3 + ….+ x la puterea n */
-        System.out.print("x= ");
+
+        System.out.print("x = ");
         Scanner in = new Scanner(System.in);
         int x = in.nextInt();
 
-        System.out.print("n= ");
+        System.out.print("n = ");
         Scanner in1 = new Scanner(System.in);
         int n = in1.nextInt();
 
-        double sum=0;
+        long sum = 1L;
+        long pow = 1L;
 
-        for(int i=0; i<=n; i++){
-            sum+=Math.pow(x,i);
+        for (int i = 0; i < n; i++) {
+            pow = pow * x;
+            sum += pow;
         }
-        System.out.println(sum);
 
+        System.out.println(sum);
     }
 }
